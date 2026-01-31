2fcdb5ac3de713ccd8dbad26a8acac2840089538
Boryss2 <159131837+Boryss2@users.noreply.github.com>
2026-01-31 12:10:45 +0100

    Update scrapingscript.py

--- scrapingscript.py
+++ scrapingscript.py
@@ -18,7 +18,8 @@ service = Service(chrome_driver_path)
 
 # Initialize Chrome WebDriver with headless option
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.binary_location = "/usr/local/bin/headless_shell"
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
@@ -26,6 +27,7 @@ chrome_options.add_argument('--remote-debugging-port=9222')
 chrome_options.add_argument('--disable-extensions')
 chrome_options.add_argument('--disable-plugins')
 chrome_options.add_argument('--window-size=1920,1080')
+
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Connect to SQLite database
